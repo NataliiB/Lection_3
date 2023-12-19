4a2bc45465ad675f0924afa0acc6b5320f4e1010
Natalia <YOUR@EMAIL.com>
2023-12-19 02:25:19 +0100

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,31 @@ import {
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
+router.get("/", (req, res) => {
+  res.json(userService.getUsers);
+});
+router.get("/:id", (req, res) => {
+  if (userService.findUserbyId(parseInt(req.body))) {
+    res.json(userService.findUserbyId(parseInt(req.body)));
+  } else {
+    res.sendStatus(400);
+  }
+});
+router.post("/", function (req, res) {
+  const result = userService.saveData(req.body);
+  res.send("Data is saved");
+  if (result) {
+  } else {
+    res.status(400).send("User wasn't sent");
+  }
+});
+router.delete("/:id", (req, res) => {
+  const foundUser = userService.findUserbyId(req.body);
 
-// TODO: Implement route controllers for user
-
+  if (foundUser) {
+    userService.deleteData(req.body);
+  } else {
+    res.sendStatus(400);
+  }
+});
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,7 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
+  getUsers(){return userRepository.getAll()?userRepository.getAll():null};
   // TODO: Implement methods to work with user
 
   search(search) {
@@ -10,6 +11,17 @@ class UserService {
     }
     return item;
   }
+  findUserbyId(idData){userRepository.getAll().some(user => user.id === idData)}
+  getData(data){data?data:null}
+  saveData = data => {
+    if (data) {
+      const isSaved = userRepository.create(data);
+      return isSaved;
+    } else {
+      return null;
+    }
+  }
+  deleteData(idData){userRepository.deleteData(idData)}
 }
 
 const userService = new UserService();
